06ec8e1d7653cb35730581b4f7c016e35aaeaf10
gabriel <110850579+gabriel-massambani@users.noreply.github.com>
2023-06-24 07:39:52 -0300

    Form

---
+++ src/app/candidato/candidato.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidato',
+  templateUrl: './candidato.component.html',
+  styleUrls: ['./candidato.component.css']
+})
+export class CandidatoComponent {
+
+  nome: string = '';
+  telUm: string = '';
+  telDois: string = '';
+  emailUm: string = '';
+  documento: string = '';
+  estado: string = '';
+  cidade: string = '';
+  bairro: string = '';
+  emailDois: string = '';
+  EstCivil: string = '';
+  habilitacao: string = '';
+  veicProprio: string = '';
+  linkCurriculoUm: string = '';
+  linkCurriculoDois: string = '';
+  linkCurriculoTres: string = '';
+  vaga: string = '';
+  areaInt: string = '';
+  hierarquia: string = '';
+  idioma: string = '';
+  outrosCursos: string = '';
+  formAcademica: string = '';
+  cursoSup: string = '';
+  cursoPos: string = '';
+  outroSeletiva: string = '';
+  dtLink1: string = '';
+  dtLink2: string = '';
+  dtLink3: string = '';
+  dtLink4: string = '';
+  obsGeral: string = '';
+
+}
